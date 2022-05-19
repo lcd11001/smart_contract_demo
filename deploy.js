e90f62df6079e0ebdd64a6c8fc696eff347ae701
Luong Cong Dan <dan.luongcong@gameloft.com>
2022-05-19 23:16:23 +0700

    - udpate deploy

--- deploy.js
+++ deploy.js
@@ -1,6 +1,6 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
-const { interface, bytecode } = require('./compile');
+const { abi, evm } = require('./compile');
 
 const provider = new HDWalletProvider(
     `${process.env.MNEMONIC}`,
@@ -13,13 +13,13 @@ const deploy = async () =>
 {
     const accounts = await web3.eth.getAccounts();
 
-    console.log('Attempting to deploy from account ', accounts[0]);
+    console.log('Attempting to deploy from account', accounts[0]);
 
-    const result = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({ data: bytecode, arguments: [''] })
+    const result = await new web3.eth.Contract(abi)
+        .deploy({ data: evm.bytecode.object, arguments: [`deployed from account ${accounts[0]}`] })
         .send({ gas: '1000000', from: accounts[0] });
 
-    console.log('Contract deployed to ', result.options.address);
+    console.log('Contract deployed to', result.options.address);
     console.log(`Scan contract at ${process.env.RINKEBY_SCAN}/${result.options.address}`);
 
     provider.engine.stop();
