88a5b91bf6f54475f17dd2f54d323c0bee7640fa
Luong Cong Dan <dan.luongcong@gameloft.com>
2022-05-19 10:49:34 +0700

    + compile

--- compile.js
+++ compile.js
@@ -1,1 +1,12 @@
-// compile code will go here
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc')
+
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const inboxSource = fs.readFileSync(inboxPath, 'utf-8');
+
+// console.log(solc.compile(inboxSource, 1));
+
+const inboxCompiled = solc.compile(inboxSource, 1);
+// console.log(inboxCompiled);
+module.exports = inboxCompiled.contracts[':Inbox'];
