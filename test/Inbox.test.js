4089f191e079daa190f6036b51ae0f4be22d6241
Luong Cong Dan <dan.luongcong@gameloft.com>
2022-05-19 23:12:19 +0700

    - update test

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
-const ganache = require('ganache-cli');
+const ganache = require('ganache');
 const Web3 = require('web3');
-const { interface, bytecode } = require('../compile');
+const { abi, evm } = require('../compile');
 
 const web3 = new Web3(ganache.provider());
 
@@ -29,8 +29,8 @@ beforeEach(async () =>
     accounts = await web3.eth.getAccounts();
 
     // Use one of those accounts to deploy the contract
-    inbox = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({ data: bytecode, arguments: [INIT_MESSAGE] })
+    inbox = await new web3.eth.Contract(abi)
+        .deploy({ data: evm.bytecode.object, arguments: [INIT_MESSAGE] })
         .send({ from: accounts[0], gas: '1000000' });
 });
 
