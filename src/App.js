fb2b3a466cb04e41dbceb443aaa596fc4e1b987a
Luong Cong Dan <dan.luongcong@gameloft.com>
2022-05-25 11:33:22 +0700

    + get players + get balance

--- src/App.js
+++ src/App.js
@@ -7,12 +7,24 @@ function App()
     console.log('web3.version', web3.version);
 
     const [manager, setManager] = useState('')
+    const [players, setPlayers] = useState([])
+    const [balance, setBalance] = useState('')
 
     useEffect(() =>
     {
         lottery.methods.manager().call().then(manager =>
         {
             setManager(manager);
+        })
+
+        lottery.methods.getPlayers().call().then(players =>
+        {
+            setPlayers(players);
+        })
+
+        web3.eth.getBalance(lottery.options.address).then(balance =>
+        {
+            setBalance(balance);
         })
     }, [])
 
@@ -21,6 +33,7 @@ function App()
         <div>
             <h2>Lottery Contract</h2>
             <p>This contract is managed by {manager}</p>
+            <p>There are currently {players.length} people entered, competing to win {web3.utils.fromWei(balance, 'ether')} ether!</p>
         </div>
     );
 }
