7c82b9369cb6ba3f24820128c35607ce1446ede6
Luong Cong Dan <dan.luongcong@gameloft.com>
2022-05-25 16:08:05 +0700

    - enter - pick winner

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App()
     const [manager, setManager] = useState('')
     const [players, setPlayers] = useState([])
     const [balance, setBalance] = useState('')
+    const [value, setValue] = useState(0)
+    const [message, setMessage] = useState('')
 
     useEffect(() =>
     {
@@ -28,12 +30,87 @@ function App()
         })
     }, [])
 
+    const onEtherEnterChanged = (e) =>
+    {
+        setValue(e.target.value)
+    }
+
+    const onSubmit = async (e) =>
+    {
+        e.preventDefault();
+        console.log('onSubmit')
+
+        setMessage('Waiting on transaction ...')
+
+        try
+        {
+            const accounts = await web3.eth.getAccounts();
+            const transaction = await lottery.methods.enter().send({
+                from: accounts[0],
+                value: web3.utils.toWei(value, 'ether')
+            })
+
+            console.log('transaction success', transaction)
+            setMessage(`transaction sucess ${transaction.transactionHash}`);
+        }
+        catch(err)
+        {
+            console.log('transaction fail', err)
+            setMessage(`transaction fail ${err.message ? err.message : err}`)
+        }
+    }
+
+    const onPickWinnerClicked = async (e) => {
+        console.log('onPickWinnerClicked')
+
+        setMessage('Waiting on picking winner ...')
+
+        try
+        {
+            const accounts = await web3.eth.getAccounts();
+            const transaction = await lottery.methods.pickWinner().send({
+                from: accounts[0]
+            })
+
+            console.log('transaction success', transaction)
+            setMessage(`A winner has been picked ${transaction.transactionHash}`);
+        }
+        catch(err)
+        {
+            console.log('transaction fail', err)
+            setMessage(`transaction fail ${err.message ? err.message : err}`)
+        }
+    }
+
 
     return (
         <div>
             <h2>Lottery Contract</h2>
             <p>This contract is managed by {manager}</p>
             <p>There are currently {players.length} people entered, competing to win {web3.utils.fromWei(balance, 'ether')} ether!</p>
+            
+            <hr />
+            
+            <form onSubmit={onSubmit}>
+                <h4>Want to try your luck?</h4>
+                <div>
+                    <label>Amount of ether to enter</label>
+                    <input
+                        value={value}
+                        onChange={onEtherEnterChanged}
+                    />
+                </div>
+                <button type="submit">Enter</button>
+            </form>
+            
+            <hr />
+
+            <h4>Ready to pick a winner</h4>
+            <button onClick={onPickWinnerClicked}>Pick a winner</button>
+
+            <hr />
+            
+            <p>{message}</p>
         </div>
     );
 }
