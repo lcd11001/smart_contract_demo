1452607f11a99c8708f3c64628eac49af2597eec
Luong Cong Dan <dan.luongcong@gameloft.com>
2022-05-25 10:18:50 +0700

    - get web3 accounts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import web3 from "./Utils/w3";
 
-function App() {
+function App()
+{
   console.log('web3.version', web3.version);
+  
+
+    web3.eth.getAccounts().then(console.log);
+  
+
   return (
     <div>
       Hello Smart Contract
