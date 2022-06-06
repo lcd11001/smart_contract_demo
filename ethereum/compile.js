20b97dd0d98cc3f4a0268b020f519ca0e833f768
Luong Cong Dan <dan.luongcong@gameloft.com>
2022-06-06 11:30:33 +0700

    + compile and write to JSON file

---
+++ ethereum/compile.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const fs = require('fs-extra');
+const solc = require('solc');
+
+const buildPath = path.resolve(__dirname, 'build');
+fs.removeSync(buildPath);
+
+const campaignPath = path.resolve(__dirname, 'contracts', 'Campaign.sol');
+const campaignSource = fs.readFileSync(campaignPath, 'utf-8');
+
+const input = {
+    language: 'Solidity',
+    sources: {
+        'Campaign.sol': {
+            content: campaignSource
+        }
+    },
+    settings: {
+        outputSelection: {
+            '*': {
+                '*': ['*']
+            }
+        }
+    }
+}
+
+const result = solc.compile(JSON.stringify(input));
+const contracts = JSON.parse(result).contracts;
+const output = contracts['Campaign.sol'];
+
+console.log(output);
+
+fs.ensureDirSync(buildPath);
+
+for (let contract in output)
+{
+    fs.outputJSONSync(
+        path.resolve(buildPath, contract + '.json'),
+        output[contract],
+        {
+            spaces: 2
+        }
+    );
+}
---
+++ ethereum/contracts/Campaign.sol
@@ -0,0 +1,108 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.14;
+
+contract CampaignFactory
+{
+    address[] public deployedCampaigns;
+
+    function createCampaign(uint minimum) public
+    {
+        Campaign newCampaign = new Campaign(minimum, msg.sender);
+        deployedCampaigns.push(address(newCampaign));
+    }
+
+    function getDeployedCampaigns() public view returns(address[] memory)
+    {
+        return deployedCampaigns;
+    }
+}
+
+contract Campaign 
+{
+    struct Request {
+        string description;
+        uint value;
+        address payable recipient;
+        bool complete;
+        uint approvalCount;
+        mapping(address => bool) approvals;
+    }
+
+    address public manager;
+    uint public minimumContribution;
+    // address[] public approvers;
+    mapping(address => bool) public approvers;
+    uint public approversCount;
+    Request[] public requests;
+
+    modifier restricted()
+    {
+        require(msg.sender == manager, "Only manager can execute this function!");
+        _;
+    }
+
+    constructor(uint minimum,  address creator)
+    {
+        manager = creator;
+        minimumContribution = minimum;
+    }
+
+    function contribute() public payable
+    {
+        require(msg.value >= minimumContribution, "Minimum contribution value required");
+        // approvers.push(msg.sender);
+        approvers[msg.sender] = true;
+        approversCount ++;
+    }
+
+    function createRequest(string memory description, uint value, address payable recipient) public restricted
+    {
+        /*
+        Request memory newRequest = Request({
+            description: description,
+            value: value,
+            recipient: recipient,
+            complete: false,
+            approvalCount: 0
+        });
+
+        requests.push(newRequest);
+        */
+        Request storage newRequest = requests.push();
+        newRequest.description = description;
+        newRequest.value = value;
+        newRequest.recipient = recipient;
+        newRequest.complete = false;
+        newRequest.approvalCount = 0;
+    }
+
+    function approveRequest(uint index) public
+    {
+        // save cost
+        Request storage request = requests[index];
+
+        // valid contributor
+        require(approvers[msg.sender], "You has not contributed yet!");
+
+        // not allow vote twice
+        require(!request.approvals[msg.sender], "You has voted already!");
+
+        request.approvalCount++;
+        request.approvals[msg.sender] = true;
+    }
+
+    function finalizeRequest(uint index) public restricted
+    {
+        // save cost
+        Request storage request = requests[index];
+
+        // not complete request
+        require(!request.complete, "Request has completed!");
+
+        // at least 50% approved
+        require(request.approvalCount > (approversCount / 2), "The request must have at least 50% approved!");
+
+        request.recipient.transfer(request.value);
+        request.complete = true;
+    }
+}
