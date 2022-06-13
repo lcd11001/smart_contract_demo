cc7bc7afd209018614644a6fcd933e93b6c6a65b
Luong Cong Dan <dan.luongcong@gameloft.com>
2022-06-13 10:33:52 +0700

    + NextJS

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Show from "./show";
+
+export default () =>
+{
+    return (
+        <Show />
+    )
+}
---
+++ pages/newcampaign.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default () =>
+{
+    return (
+        <h1>New Campaign Page</h1>
+    )
+}
