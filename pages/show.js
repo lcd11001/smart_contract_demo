5b7020c89eeb6ce4875e58494fceb84bc5bcb14a
Luong Cong Dan <dan.luongcong@gameloft.com>
2022-06-13 15:32:52 +0700

    * testing list contract

--- pages/show.js
+++ pages/show.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useEffect, useState, Fragment, useCallback } from "react";
+import CampaignFactory from '../ethereum/src/CampaignFactory'
 
 export default () =>
 {
+    const [deployedCampaigns, setDeployedCampaigns] = useState([])
+
+    const getDeployedCampaigns = useCallback(async () =>
+    {
+        const campaigns = await CampaignFactory.methods.getDeployedCampaigns().call()
+        console.log('campaigns', campaigns)
+        setDeployedCampaigns(campaigns)
+    }, [])
+
+    useEffect(() =>
+    {
+        getDeployedCampaigns()
+    }, [getDeployedCampaigns])
+
     return (
-        <h1>Show Page</h1>
+        <Fragment>
+            <h1>Show Page</h1>
+            {
+                deployedCampaigns.map((campaign, index) => (
+                    <p key={index}>{campaign}</p>
+                ))
+            }
+        </Fragment>
     )
 }
