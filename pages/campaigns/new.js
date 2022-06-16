1d2d98e1e8c13cb30a4689875e99661ad7425e08
Luong Cong Dan <dan.luongcong@gameloft.com>
2022-06-16 17:31:43 +0700

    - on minimum change

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { Form, Button, Label, Input } from 'semantic-ui-react';
 
 import Layout from '../../components/layout'
 
 const NewCampaign = () =>
 {
+    const [minimum, setMinimum] = useState('0')
+
+    const onMinimumChanged = (e, data) =>
+    {
+        setMinimum(data.value)
+    }
+
     return (
         <Layout>
-            <h1>New Campaign Page</h1>
+            <h3>Create New Campaign</h3>
+            <Form>
+                <Form.Field>
+                    <label>Minimum Contributor</label>
+                    <Input
+                        label="wei"
+                        labelPosition="right"
+                        type="number"
+                        value={minimum}
+                        onChange={onMinimumChanged}
+                    />
+                </Form.Field>
+                <Button primary type="submit">Submit</Button>
+            </Form>
         </Layout>
     )
 }
