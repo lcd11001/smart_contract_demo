3276cd7a0450b1e425ae63ade32acbc3da58bd5e
Luong Cong Dan <dan.luongcong@gameloft.com>
2022-06-16 14:12:14 +0700

    + Semantic Card Group

--- pages/show.js
+++ pages/show.js
@@ -1,15 +1,26 @@
 import React, { useEffect, useState, Fragment, useCallback } from "react";
 import CampaignFactory from '../ethereum/src/CampaignFactory'
+import { Card } from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css';
 
 const Show = ({ campaigns }) =>
 {
+    const renderCampaigns = () =>
+    {
+        const items = campaigns.map(address => ({
+            header: address,
+            description: <a>View Campaign</a>,
+            fluid: true
+        }))
+
+        return <Card.Group items={items} />
+    }
+
     return (
         <Fragment>
             <h1>Show Page</h1>
             {
-                campaigns.map((campaign, index) => (
-                    <p key={index}>{campaign}</p>
-                ))
+                renderCampaigns()
             }
         </Fragment>
     )
