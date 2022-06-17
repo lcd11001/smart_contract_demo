61d25eb90bd80c311b8f25ed8839af3981db0a6e
Luong Cong Dan <dan.luongcong@gameloft.com>
2022-06-17 10:33:58 +0700

    - error message when create a new campaign

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,21 +1,46 @@
 import React, { useState } from "react";
-import { Form, Button, Label, Input } from 'semantic-ui-react';
+import { Form, Button, Message, Input } from 'semantic-ui-react';
 
 import Layout from '../../components/layout'
+
+import CampaignFatory from '../../ethereum/src/CampaignFactory'
+import web3 from '../../ethereum/src/web3'
 
 const NewCampaign = () =>
 {
     const [minimum, setMinimum] = useState('0')
+    const [errorMessage, setErrorMessage] = useState('')
 
     const onMinimumChanged = (e, data) =>
     {
         setMinimum(data.value)
     }
 
+    const onSubmit = async (e, data) =>
+    {
+        e.preventDefault()
+
+        try
+        {
+
+            const accounts = await web3.eth.getAccounts()
+
+            await CampaignFatory.methods
+                .createCampaign(minimum)
+                .send({
+                    from: accounts[0]
+                })
+        }
+        catch (err)
+        {
+            setErrorMessage(err.message)
+        }
+    }
+
     return (
         <Layout>
             <h3>Create New Campaign</h3>
-            <Form>
+            <Form onSubmit={onSubmit} error={errorMessage.length !== 0}>
                 <Form.Field>
                     <label>Minimum Contributor</label>
                     <Input
@@ -26,6 +51,9 @@ const NewCampaign = () =>
                         onChange={onMinimumChanged}
                     />
                 </Form.Field>
+
+                <Message error header="Oops!" content={errorMessage} />
+
                 <Button primary type="submit">Submit</Button>
             </Form>
         </Layout>
