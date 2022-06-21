fb3fb5530a3868e4fcc7c6d6fdbeb3b3cb2ece15
Luong Cong Dan <dan.luongcong@gameloft.com>
2022-06-21 10:44:32 +0700

    - navigation on view campaigns

--- pages/campaigns/index.js
+++ pages/campaigns/index.js
@@ -4,13 +4,19 @@ import { Card, Button } from 'semantic-ui-react'
 
 import Layout from "../../components/layout";
 
+import { Link } from '../../routes'
+
 const CampaignsIndex = ({ campaigns }) =>
 {
     const renderCampaigns = () =>
     {
         const items = campaigns.map(address => ({
             header: address,
-            description: <a>View Campaign</a>,
+            description: (
+                <Link route={`/campaigns/${address}`}>
+                    <a>View Campaign</a>
+                </Link>
+            ),
             fluid: true
         }))
 
@@ -21,13 +27,17 @@ const CampaignsIndex = ({ campaigns }) =>
         <Layout>
             <h3>Open Campaigns</h3>
             <div>
-                <Button
-                    floated="right"
-                    content="Create Campaign"
-                    icon="add circle"
-                    primary
-                    style={{marginTop: '0.875em'}}
-                />
+                <Link route="/campaigns/new">
+                    <a>
+                        <Button
+                            floated="right"
+                            content="Create Campaign"
+                            icon="add circle"
+                            primary
+                            style={{ marginTop: '0.875em' }}
+                        />
+                    </a>
+                </Link>
                 {
                     renderCampaigns()
                 }
