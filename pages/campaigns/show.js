ad53891a54503371f697ddc497c83abd145983b6
Luong Cong Dan <dan.luongcong@gameloft.com>
2022-06-21 10:52:37 +0700

    + show campaign

---
+++ pages/campaigns/show.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import Layout from "../../components/layout";
+
+const ShowCampaign = () =>
+{
+    return (
+        <Layout>
+            <h3>ShowCampaign</h3>
+        </Layout>
+    )
+}
+
+export default ShowCampaign
